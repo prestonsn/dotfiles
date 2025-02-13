b78fd24468138804808865f55a63c9cd678d5bc7
Buck McCready <jbuckmccready@gmail.com>
2025-02-12 18:39:57 -0800

    Remove unused color schemes

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -9,21 +9,4 @@ return {
 		"folke/tokyonight.nvim",
 		priority = 1000,
 	},
-	'navarasu/onedark.nvim',
-	'Shatur/neovim-ayu',
-	'Mofiqul/vscode.nvim',
-	'sainnhe/sonokai',
-	{
-		'maxmx03/solarized.nvim',
-		priority = 1000,
-	},
-	'pappasam/papercolor-theme-slim',
-	{
-		"ellisonleao/gruvbox.nvim",
-		priority = 1000,
-	},
-	{
-		'ribru17/bamboo.nvim',
-		priority = 1000,
-	}
 }

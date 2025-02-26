d392234fe9ff5ae718984dcdef4124618af5f2e7
Buck McCready <jbuckmccready@gmail.com>
2025-02-25 22:24:26 -0800

    Add autopairs fast_wrap keymap

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -3,20 +3,19 @@ return {
 	event = 'InsertEnter',
 	config = function()
 		require('nvim-autopairs').setup({
-			-- FIXME: having this enabled breaks inserting newlines with enter
-			-- fast_wrap = {
-			-- 	map = '<C-m>',
-			-- 	chars = { '{', '[', '(', '"', "'" },
-			-- 	pattern = [=[[%'%"%>%]%)%}%,]]=],
-			-- 	end_key = '$',
-			-- 	before_key = 'h',
-			-- 	after_key = 'l',
-			-- 	cursor_pos_before = true,
-			-- 	keys = 'qwertyuiopzxcvbnmasdfghjkl',
-			-- 	manual_position = true,
-			-- 	highlight = 'Search',
-			-- 	highlight_grey = 'Comment'
-			-- }
+			fast_wrap = {
+				map = '<C-e>',
+				chars = { '{', '[', '(', '"', "'" },
+				pattern = [=[[%'%"%>%]%)%}%,]]=],
+				end_key = '$',
+				before_key = 'h',
+				after_key = 'l',
+				cursor_pos_before = true,
+				keys = 'qwertyuiopzxcvbnmasdfghjkl',
+				manual_position = true,
+				highlight = 'Search',
+				highlight_grey = 'Comment'
+			}
 		})
 	end
 }

a1e4ff496afb47ac4f0b72cfe8d59f1bd264f6ac
Buck McCready <jbuckmccready@gmail.com>
2025-03-27 17:05:47 -0700

    chore(nvim-cmp): comment out <Tab> completion to reserve for copilot
    Commented out the <Tab> key mapping for completion in nvim-cmp to reserve it for copilot usage.

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -31,10 +31,11 @@ return {
 				["<C-u>"] = cmp.mapping.scroll_docs(-4),
 				["<C-d>"] = cmp.mapping.scroll_docs(4),
 				["<C-e>"] = cmp.mapping.close(),
-				["<Tab>"] = cmp.mapping.confirm({
-					behavior = cmp.ConfirmBehavior.Insert,
-					select = true,
-				}),
+				-- reserve <Tab> completion for copilot
+				-- ["<Tab>"] = cmp.mapping.confirm({
+				-- 	behavior = cmp.ConfirmBehavior.Insert,
+				-- 	select = true,
+				-- }),
 				["<CR>"] = cmp.mapping.confirm({
 					behavior = cmp.ConfirmBehavior.Insert,
 					select = true,

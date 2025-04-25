f193e93bfdc2d94ccd51c7f1a0a30076fae9cdbd
Buck McCready <jbuckmccready@gmail.com>
2025-04-25 00:47:16 -0700

    Switch from nvim-cmp to blink-cmp

---
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,66 @@
+-- Auto-completion:
+return {
+	'saghen/blink.cmp',
+	dependencies = 'LuaSnip',
+	build = 'cargo +nightly build --release',
+	event = 'InsertEnter',
+	opts = {
+		keymap = {
+			['<CR>'] = { 'accept', 'fallback' },
+			['<C-Space>'] = {},
+			['<C-n>'] = { 'select_next', 'show' },
+			['<C-p>'] = { 'select_prev' },
+			['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
+			['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
+		},
+		signature = { enabled = true },
+		completion = {
+			list = {
+				selection = { preselect = false, auto_insert = false },
+				-- Show more items than default 10 so it's easier to browse apis.
+				max_items = 100,
+			},
+			documentation = {
+				auto_show = true,
+				-- Decrease the default delay to show the documentation.
+				auto_show_delay_ms = 50,
+				window = {
+					border = 'rounded',
+				},
+			},
+		},
+		snippets = { preset = 'luasnip' },
+		-- Disable command line completion:
+		cmdline = { enabled = false },
+		sources = {
+			-- Disable some sources in comments and strings.
+			default = function()
+				local sources = { 'lsp', 'buffer' }
+				local ok, node = pcall(vim.treesitter.get_node)
+
+				if ok and node then
+					if not vim.tbl_contains({ 'comment', 'line_comment', 'block_comment' }, node:type()) then
+						table.insert(sources, 'path')
+					end
+					if node:type() ~= 'string' then
+						table.insert(sources, 'snippets')
+					end
+				end
+
+				return sources
+			end,
+		},
+		appearance = {
+			kind_icons = require('icons').symbol_kinds,
+		},
+	},
+	config = function(_, opts)
+		require('blink.cmp').setup(opts)
+
+		-- Set border color.
+		vim.api.nvim_set_hl(0, 'BlinkCmpDocBorder', { link = 'FloatBorder' })
+
+		-- Extend neovim's client capabilities with the completion ones.
+		vim.lsp.config('*', { capabilities = require('blink.cmp').get_lsp_capabilities(nil, true) })
+	end,
+}

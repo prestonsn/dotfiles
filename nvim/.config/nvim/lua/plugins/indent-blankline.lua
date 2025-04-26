2a7f3309758de2ae1f0536db713325b8b39b2c51
Buck McCready <jbuckmccready@gmail.com>
2025-04-25 18:33:18 -0700

    feat(indent-blankline): remove rainbow highlight

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -3,38 +3,15 @@ return {
 	'lukas-reineke/indent-blankline.nvim',
 	main = 'ibl',
 	event = 'VeryLazy',
-	opts = function()
-		local highlight = {
-			"RainbowRed",
-			"RainbowYellow",
-			"RainbowBlue",
-			"RainbowOrange",
-			"RainbowGreen",
-			"RainbowViolet",
-			"RainbowCyan",
-		}
-
-		local hooks = require('ibl.hooks')
-		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-			vim.api.nvim_set_hl(0, 'RainbowRed', { link = 'RainbowDelimiterRed' })
-			vim.api.nvim_set_hl(0, 'RainbowYellow', { link = 'RainbowDelimiterYellow' })
-			vim.api.nvim_set_hl(0, 'RainbowBlue', { link = 'RainbowDelimiterBlue' })
-			vim.api.nvim_set_hl(0, 'RainbowOrange', { link = 'RainbowDelimiterOrange' })
-			vim.api.nvim_set_hl(0, 'RainbowGreen', { link = 'RainbowDelimiterGreen' })
-			vim.api.nvim_set_hl(0, 'RainbowViolet', { link = 'RainbowDelimiterViolet' })
-			vim.api.nvim_set_hl(0, 'RainbowCyan', { link = 'RainbowDelimiterCyan' })
-		end)
-
-		return {
-			debounce = 50,
-			indent = {
-				char = require('icons').misc.vertical_bar,
-				highlight = highlight,
-			},
-			scope = {
-				show_start = false,
-				show_end = false,
-			},
-		}
-	end,
+	opts =
+	{
+		debounce = 50,
+		indent = {
+			char = require('icons').misc.vertical_bar,
+		},
+		scope = {
+			show_start = false,
+			show_end = false,
+		},
+	},
 }
